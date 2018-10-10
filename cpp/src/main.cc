c5501082d22b71517f97b57298b5dbaaabec14bf
Animesh Trivedi <atrivedi@apache.org>
2018-10-10 16:06:31 +0200

    arrow building in c++

--- cpp/src/main.cc
+++ cpp/src/main.cc
@@ -1,5 +1,17 @@
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <arrow/array.h>
+#include <arrow/memory_pool.h>
+
 #include <iostream>
 int main(int argc, char **argv) {
-  std::cout << __cplusplus << std::endl;
+  auto memory_pool = arrow::default_memory_pool();
+  std::cout << "__cplusplus version is : " << __cplusplus << std::endl;
+  std::cout << " address of memory pool is " << memory_pool << std::endl;
   return 0;
 }
