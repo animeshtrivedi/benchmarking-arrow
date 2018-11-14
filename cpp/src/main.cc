3467ac95035a1f691c512ba29080ab7abc534f0e
Animesh Trivedi <atrivedi@apache.org>
2018-11-14 11:36:33 +0100

    [c++] add new files in the build and debug option
    Signed-off-by: Animesh Trivedi <atrivedi@apache.org>

--- cpp/src/main.cc
+++ cpp/src/main.cc
@@ -18,8 +18,14 @@
 #include "common.h"
 #include "arrow-reader-example.h"
 #include "arrow-reader.h"
+#include "PeakPerformance.h"
+#include "CLikeRoutine.h"
 
 int main(int argc, char **argv) {
+    std::cout<<"--------------------------------------------------------------------------\n";
+    std::cout<<" usage: \"filename\" (which arrow file to open)] \"d\" (run in the debug mode) \n";
+    std::cout<<"--------------------------------------------------------------------------\n";
+    // this is example code
 #if 0
   ArrowReadExample *ex = new ArrowReadExample("/");
   std::cout << "location of the object is " << ex << "\n";
@@ -27,8 +33,11 @@ int main(int argc, char **argv) {
   s = ex->debug_show();
   s = ex->read();
 #endif
+
+#if 1
+  // this is benchmark code
     ArrowReader *r = NULLPTR;
-  if(argc == 2){
+  if(argc > 1){
       // use the first one as the file name
       std::cout<<"Opening the file : " << argv[1] << "\n";
       r = new ArrowReader(argv[1]);
@@ -36,15 +45,32 @@ int main(int argc, char **argv) {
       std::cout<<"Opening the _DEFAULT_ file : /home/atr/zrl/external/github/animeshtrivedi/arrow-on-crail/data/f100-ss-p15.arrow" "\n";
       r = new ArrowReader("/home/atr/zrl/external/github/animeshtrivedi/arrow-on-crail/data/f100-ss-p15.arrow");
   }
-
+#endif
+//#if 1
+//  PeakPerformance *r = new PeakPerformance(atol(argv[1]), atol(argv[2]));
   r->init();
-  r->run();
-  std::cout << r->summary() << "\n";
-  long csum = r->getChecksum();
-  if( csum != 303414922182153){
-    std::cout<<"Checksum _DOES_NOT_ match " << "\n";
-  } else{
-    std::cout<<"Checksum _MATCHES_ " << "\n";
+  if(argc > 2 && strcasecmp(argv[2], "d") == 0){
+      std::cout<<"Running _with_ debug \n";
+      r->runWithDebug();
+  } else {
+      std::cout<<"Running _with_out_ debug \n";
+      r->run();
   }
+
+  std::cout << r->summary() << "\n";
+
+//#else
+//  CLikeRoutine *r = new CLikeRoutine();
+//  r->run();
+//#endif
+//
+//#if 0
+//  long csum = r->getChecksum();
+//  if( csum != 303414922182153){
+//    std::cout<<"Checksum _DOES_NOT_ match " << "\n";
+//  } else{
+//    std::cout<<"Checksum _MATCHES_ " << "\n";
+//  }
+//#endif
   return 0;
 }
